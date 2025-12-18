b981f43ac1ee8e5738cbea375d38e0fd447c4981
Ragelexsuss <emanoelbilly0@gmail.com>
2025-12-18 20:27:15 +0700

    Update web to do list

---
+++ Website Todo List/js/script.js
@@ -0,0 +1,79 @@
+const button = document.querySelector('.add_list');
+const form = document.querySelector('.memo-form');
+const modalmemo= document.querySelector('.modal-memo');
+const listmemo = document.querySelector('.list-memo')
+
+
+button.onclick = () => {
+    modalmemo.classList.toggle('active');
+}
+
+document.addEventListener('click', function(e) {
+    if (!button.contains(e.target) && !form.contains(e.target)) {
+        modalmemo.classList.remove('active');
+    }
+})
+function checkmemo(){
+    if (!listmemo.children.length) {
+        const p = document.createElement('p');
+        p.textContent = 'Silahkan Isi Memo';
+        listmemo.append(p);
+
+    }
+}
+
+
+function buatmemo(){
+    const time = new Date();
+
+    const judulmemo = document.querySelector('#judul-memo');
+    const isimemo = document.querySelector('#text-memo');
+
+    const stringjudul = judulmemo.value.trim();
+    const stringmemo = isimemo.value.trim();
+
+    const listmemo = document.querySelector('.list-memo')
+
+    if (stringjudul === '' && stringmemo === '') {
+        alert("Silahkan Isi Judul dan Isi Memo");
+        return;
+    }
+
+    const div = document.createElement('div');
+    div.classList.add('card-memo');
+
+    // judul Memo
+    const judul = document.createElement('div');
+    judul.classList.add('judul-memo');
+
+    const title = document.createElement('h3');
+    title.textContent = stringjudul;
+
+    const waktu = document.createElement('p');
+    waktu.textContent = `Created At ${time.toLocaleTimeString()}`;
+    judul.append(title, waktu)
+
+    // Isi Memo
+    const description = document.createElement('p');
+    description.textContent = stringmemo;
+
+    // Button Hapus
+    const hapus = document.createElement('Button');
+    hapus.textContent = "Hapus Memo";
+    modalmemo.classList.remove('active');
+
+    hapus.addEventListener('click', (e) => {
+        e.target.parentElement.remove()
+        checkmemo()
+    })
+
+    div.append(judul,description, hapus);
+    listmemo.append(div);
+
+    judulmemo.value="";
+    isimemo.value="";
+
+    document.getElementById('reminder').remove()
+
+
+}

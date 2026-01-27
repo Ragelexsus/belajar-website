ad05bdd62fd1cb9bed966bb44b33a97700fd7407
Ragelexsuss <emanoelbilly0@gmail.com>
2026-01-27 16:18:53 +0700

    update users

--- Katalog toko/src/controller/users.js
+++ Katalog toko/src/controller/users.js
@@ -1,5 +1,17 @@
+import usermodel from "../model/users.js"
+
 const getalluser = async (req, res)=>{
-    res.send('Hello World!');
+    try{
+        const row  = await  usermodel.searchrole('user')
+        res.json({
+            message :"Success get role",
+            data: row,
+        })
+    }catch (e){
+        res.status(400).json({
+            message:e.message,
+        })
+    }
 }
 
 export default {
--- Katalog toko/src/model/users.js
+++ Katalog toko/src/model/users.js
@@ -1,9 +1,32 @@
 import pool from '../config/database.js'
+import bcrypt from 'bcrypt'
 
-const CreateNewUser = () =>{
+const CreateNewUser = async (username, password, role) =>{
     try{
+        const hashPassword = await HashPassword(password);
+        const [rows] = await pool.execute(`INSERT INTO user_admin(username,password,id_role) values (?,?,?)`, [username, hashPassword, role]);
 
     }catch (e){
         throw e.message;
     }
+}
+
+const HashPassword = async (plain_password) =>{
+    const saltRounds = 10;
+    return await bcrypt.hash(plain_password, saltRounds);
+}
+
+const searchrole = async (role)=>{
+    try{
+        const [rows] = await pool.execute(`SELECT * FROM role_admin where nama_role like '%${role}%'`)
+        return rows;
+
+    } catch (e){
+        throw e.message;
+    }
+
+}
+
+export default{
+    searchrole
 }
